4f3736b9c89bf1f265d0339717433cefa5128139
Stepzero.Solutions <info@stepzero.solutions>
2017-04-24 22:38:02 +1200

    First release

---
+++ registration.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Affiliate (http://stepzero.solutions/)
+ *
+ * Module
+ *
+ * PHP version 5
+ *
+ * @category Module
+ * @package  Affiliate
+ * @author   Don Nuwinda <nuwinda@gmail.com>
+ * @license  GPL http://stepzero.solutions
+ * @link     http://stepzero.solutions
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Stepzerosolutions_Affiliate',
+    __DIR__
+);
